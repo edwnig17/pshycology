a5ed7377eb149e9542defe758bdf4f425960db4b
EdwingStivenMejiaSaenzCampus <edwingstiven20@gmail.com>
2023-06-14 13:31:25 -0500

    first commit

---
+++ psychology/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once "controllers/template.controller.php";
+
+$index = new TemplateController();
+
+$index -> index();
+
+?>
---
+++ psychology/views/modules/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 1.0
+    <strong>Copyright &copy; <?php echo date("Y")  ?> <a href="https://adminlte.io">Psychology 1.0</a>.</strong> All rights reserved.
+  </footer>
